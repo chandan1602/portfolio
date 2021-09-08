c8e786bc0d52b2e61e642957f0d8408ef10a10e1
chandan1602 <cbansal82@gmail.com>
2021-09-08 10:27:40 +0530

    Portfolio updated

--- js/index.js
+++ js/index.js
@@ -18,3 +18,19 @@ let trans = () => {
         document.documentElement.classList.remove('transition');
     }, 0)
 }
+
+$("#footer a[href]").click(function (e) {
+    e.preventDefault();
+    if (this.href) {
+        var target = this.href;
+        let w = window.innerWidth*0.9
+        let h = window.innerHeight*0.9
+        let mt = window.innerHeight - h
+        let ml = window.innerWidth - w
+        if(target.slice(-6)!=="#modal") {
+            setTimeout(function () {
+                window.open(target, "_blank", `toolbar=yes,scrollbars=yes,resizable=yes,top=${mt},left=${ml},width=${w},height=${h}`);
+            }, 500);
+        }
+    }
+});
