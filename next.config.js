7090a56ea8e52532cf87ea52e4bdeded01710dfa
chandan1602 <cbansal82@gmail.com>
2021-12-16 01:08:02 +0530

    Travis support added

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withImages = require("next-images");
+const { withPlugins } = require("next-compose-plugins");
+
+const nextConfig = {
+  assetPrefix: '.',
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+      },
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+};
+
+module.exports = withPlugins([withImages], nextConfig);
+
