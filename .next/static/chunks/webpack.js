91b87db38d33b18316f90af9de267b2a7cc360cf
chandan1602 <cbansal82@gmail.com>
2021-12-18 17:59:04 +0530

    Netlify added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fb133922bd6bd9f5"; }
+/******/ 		__webpack_require__.h = function() { return "076ad171195bc003"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## The Site has been moved
-New Site is Live on Vercel.
-[Visit Now](https://portfolio-chandan1602.vercel.app/)
+New Site is Live on Netlify.
+[Visit Now](https://chandanbansal.netlify.app/)
