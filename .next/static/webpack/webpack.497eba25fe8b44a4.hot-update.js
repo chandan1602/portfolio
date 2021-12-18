1d9987a48b392b8998c41e7993ac707e616e7dce
chandan1602 <cbansal82@gmail.com>
2021-12-18 17:59:04 +0530

    Netlify added

--- .next/static/webpack/webpack.497eba25fe8b44a4.hot-update.js
+++ .next/static/webpack/webpack.497eba25fe8b44a4.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "8333d126555cb3d7"; }
+/******/ 	__webpack_require__.h = function() { return "076ad171195bc003"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
