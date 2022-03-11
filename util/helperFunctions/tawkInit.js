4b0be2f46797c036e22526293886a1b50e681b14
chandan1602 <cbansal82@gmail.com>
2022-03-12 00:01:58 +0530

    Updated Info

---
+++ util/helperFunctions/tawkInit.js
@@ -0,0 +1,11 @@
+export const tawkInit = () => {
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5e721acaeec7650c3320d213/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+}
