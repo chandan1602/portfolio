157d8e33be68762c8ff6ccc239fbeb111e1e9d51
chandan1602 <cbansal82@gmail.com>
2022-12-28 04:22:53 +0530

    Hotfix

--- components/TextButton/button.js
+++ components/TextButton/button.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import styles from './../../styles/TextButton.module.css'
-
+import styles from './../../styles/textButton.module.css'
 const TextButton = (props) => {
     return(
     <div className={styles.main}>
