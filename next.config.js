7fcf8deee2e5a0c456f28e183c2af2d4833486e6
chandan1602 <cbansal82@gmail.com>
2022-12-28 04:15:37 +0530

    Blog Functionality V1

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   reactStrictMode: true,
   // assetPrefix: '.'
   env: {
-    SERVER_URL : 'http://ec2-13-127-58-63.ap-south-1.compute.amazonaws.com:3000',
+    SERVER_URL : 'http://ec2-3-110-191-56.ap-south-1.compute.amazonaws.com:3000',
     DEV_URL : 'http://localhost:3002'
   }
 }
