74651eb123a303170fa2e84aee68646c365cc63e
chandan1602 <cbansal82@gmail.com>
2023-01-10 15:55:51 +0530

    Server http to https:'

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   reactStrictMode: true,
   // assetPrefix: '.'
   env: {
-    SERVER_URL : 'http://ec2-3-110-191-56.ap-south-1.compute.amazonaws.com:3000',
+    SERVER_URL : 'https://ec2-3-110-191-56.ap-south-1.compute.amazonaws.com:3000',
     DEV_URL : 'http://localhost:3002'
   }
 }
