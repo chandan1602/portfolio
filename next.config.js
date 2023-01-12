56c8790d366699bb3df07c7dac98d53b6af8b7c8
chandan1602 <cbansal82@gmail.com>
2023-01-13 00:22:42 +0530

    Solving getStaticPaths problem

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   NODE_TLS_REJECT_UNAUTHORIZED: '0',
+  target: 'serverless',
   // assetPrefix: '.'
   env: {
     SERVER_URL : 'https://ec2-3-110-191-56.ap-south-1.compute.amazonaws.com:3000',
